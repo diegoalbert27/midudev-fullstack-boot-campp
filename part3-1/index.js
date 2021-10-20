298a6107db92440bd02cf25e0e2f00c76acbbd25
dHinagas <hinagasrodriguez@gmail.com>
2021-10-20 13:50:37 -0400

    Ejercicio y practica de rest con morgan

--- part3-1/index.js
+++ part3-1/index.js
@@ -1,70 +1,25 @@
 const express = require("express")
 const app = express()
+const morgan = require("morgan")
 
-let { notes } = require("./db.json")
+const phonebook = require("./routes/phonebook.routes")
+const notes = require("./routes/notes.routes")
 
+let { persons } = require("./db.json")
+
+app.use(morgan('dev'))
 app.use(express.json())
+
+app.use('/api/phonebook', phonebook)
+app.use('/api/notes', notes)
 
 app.get('/', (req, res) => {
     res.send("Hello World");
 })
 
-app.get('/api/notes', (req, res) => {
-    res.status(200).json(notes)
-})
-
-app.get('/api/notes/:id', (req, res) => {
-    const { id } = req.params
-    const note = notes.find(note => note.id === Number(id))
-    res.status(200).json(note)
-})
-
-app.post('/api/notes', (req, res) => {
-    const note  = req.body
-
-    if (!note || !note.content) {
-        return res.status(400).json({
-            error: 'note.content is missing'
-        })
-    }
-
-    const ids = notes.map(note => note.id)
-    const maxId = Math.max(...ids)
-
-    const newNote = {
-        id: maxId + 1,
-        content: note.content,
-        date: new Date().toISOString(),
-        important: typeof note.important !== 'undefined' ? note.important : false
-    }
-
-    notes = [...notes, newNote]
-
-    res.status(201).json(newNote)
-})
-
-app.put('/api/notes/:id', (req, res) => {
-    const { id } = req.params
-    const { content, important }  = req.body
-
-    notes = notes.filter(note => note.id !== Number(id))
-    
-    const note = {
-        id: Number(id),
-        content,
-        date: new Date().toISOString(),
-        important
-    }
-
-    notes = notes.concat(note)
-
-    res.status(201).json(note)
-})
-
-app.delete('/api/notes/:id', (req, res) => {
-    const { id } = req.params
-    notes = notes.filter(note => note.id !== Number(id))
-    res.status(201).json(notes)
+app.get('/info', (req, res) => {
+    const date = new Date()
+    res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`)
 })
 
 const PORT = 3001
