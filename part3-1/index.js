b1764a6d646905a38fac042f600355524fdf1759
dHinagas <hinagasrodriguez@gmail.com>
2021-10-22 14:22:04 -0400

    Heroku server

--- part3-1/index.js
+++ part3-1/index.js
@@ -1,12 +1,20 @@
-const express = require("express")
+const express = require('express')
 const app = express()
-const morgan = require("morgan")
+const morgan = require('morgan')
+const cors = require('cors')
 
-const phonebook = require("./routes/phonebook.routes")
-const notes = require("./routes/notes.routes")
+const phonebook = require('./routes/phonebook.routes')
+const notes = require('./routes/notes.routes')
 
-let { persons } = require("./db.json")
+const PORT = 3001
 
+const { persons } = require('./db.json')
+
+// Configuration
+app.set('port', process.env.PORT || PORT)
+
+// Middleware
+app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
 
@@ -14,14 +22,12 @@ app.use('/api/phonebook', phonebook)
 app.use('/api/notes', notes)
 
 app.get('/', (req, res) => {
-    res.send("Hello World");
+  res.send('Hello World')
 })
 
 app.get('/info', (req, res) => {
-    const date = new Date()
-    res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`)
+  const date = new Date()
+  res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`)
 })
 
-const PORT = 3001
-
-app.listen(PORT, () => console.log("server listening on port 3001"))
+app.listen(app.get('port'), () => console.log(`server listening on port ${app.get('port')}`))
--- part3-1/routes/notes.routes.js
+++ part3-1/routes/notes.routes.js
@@ -1,64 +1,64 @@
-const express = require("express")
+const express = require('express')
 const router = express.Router()
 
-let { notes } = require("../db.json")
+let { notes } = require('../db.json')
 
 router.get('/', (req, res) => {
-    res.status(200).json(notes)
+  res.status(200).json(notes)
 })
 
 router.get('/:id', (req, res) => {
-    const { id } = req.params
-    const note = notes.find(note => note.id === Number(id))
-    res.status(200).json(note)
+  const { id } = req.params
+  const note = notes.find(note => note.id === Number(id))
+  res.status(200).json(note)
 })
 
 router.post('/', (req, res) => {
-    const note  = req.body
+  const note = req.body
 
-    if (!note || !note.content) {
-        return res.status(400).json({
-            error: 'note.content is missing'
-        })
-    }
+  if (!note || !note.content) {
+    return res.status(400).json({
+      error: 'note.content is missing'
+    })
+  }
 
-    const ids = notes.map(note => note.id)
-    const maxId = Math.max(...ids)
+  const ids = notes.map(note => note.id)
+  const maxId = Math.max(...ids)
 
-    const newNote = {
-        id: maxId + 1,
-        content: note.content,
-        date: new Date().toISOString(),
-        important: typeof note.important !== 'undefined' ? note.important : false
-    }
+  const newNote = {
+    id: maxId + 1,
+    content: note.content,
+    date: new Date().toISOString(),
+    important: typeof note.important !== 'undefined' ? note.important : false
+  }
 
-    notes = [...notes, newNote]
+  notes = [...notes, newNote]
 
-    res.status(201).json(newNote)
+  res.status(201).json(newNote)
 })
 
 router.put('/:id', (req, res) => {
-    const { id } = req.params
-    const { content, important }  = req.body
+  const { id } = req.params
+  const { content, important } = req.body
 
-    notes = notes.filter(note => note.id !== Number(id))
-    
-    const note = {
-        id: Number(id),
-        content,
-        date: new Date().toISOString(),
-        important
-    }
+  notes = notes.filter(note => note.id !== Number(id))
 
-    notes = notes.concat(note)
+  const note = {
+    id: Number(id),
+    content,
+    date: new Date().toISOString(),
+    important
+  }
 
-    res.status(201).json(note)
+  notes = notes.concat(note)
+
+  res.status(201).json(note)
 })
 
 router.delete('/:id', (req, res) => {
-    const { id } = req.params
-    notes = notes.filter(note => note.id !== Number(id))
-    res.status(204).end()
+  const { id } = req.params
+  notes = notes.filter(note => note.id !== Number(id))
+  res.status(204).end()
 })
 
 module.exports = router
